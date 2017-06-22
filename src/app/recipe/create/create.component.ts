32ec95297e0b7b086de78a249d40b4b45a89f321
dlewita <d.lewit@webankieta.pl>
2017-06-22 18:36:40 +0200

    favicon, tworzenie przepisu

--- src/app/recipe/create/create.component.ts
+++ src/app/recipe/create/create.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NotifierService } from "../../core/notifier.service";
+import { CreateRecipe } from '../../shared/models/recipe/new-recipe';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'fg-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.sass']
 })
-export class CreateComponent implements OnInit {
-	title = 'Create component!';
+export class CreateComponent {
+  recipe: CreateRecipe = new CreateRecipe();
 	
-  constructor() { }
+  constructor(
+      private recipes: RecipeService,
+      private notifier: NotifierService
+  ) { }
 
   ngOnInit() {
   }
 
+  addRecipe() {
+    //console.log(this.createRecipe);
+  }
 }
