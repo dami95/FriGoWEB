d961bcb37e1806eafeda619fc7a86fa9703d1c59
Dominik Ostrowski <dominik.ostrowski@live.com>
2017-06-25 16:38:15 +0200

    Change notifier time

--- src/app/core/notifier.service.ts
+++ src/app/core/notifier.service.ts
@@ -6,7 +6,7 @@ export class NotifierService {
     private notifications: Array<Object>;
 
     constructor() {
-        this.time = 5000;
+        this.time = 2000;
         this.notifications = [];
     }
 
